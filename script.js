e42eb8a18fd6a87dafa449d2e1e36d9b551da907
Gayatri <gayatridwankhade@gmail.com>
2023-10-04 17:56:01 +0530

    new changes commited

--- script.js
+++ script.js
@@ -11,3 +11,27 @@ function opentab(tabname){
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+var sideMenu = document.getElementById("sidemenu");
+function openmenu(){
+    sideMenu.style.right = "0";
+}
+function closemenu(){
+    sideMenu.style.right = "-200px";
+}
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbwvDqwJvvIhqmcPFlUwfdJfP_QIxMl20sF44jMdZbgpvAqYQhMEw2aimYlBNoTr4rRA/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        },5000)
+        form.reset()
+      }
+        )
+      .catch(error => console.error('Error!', error.message))
+  })
